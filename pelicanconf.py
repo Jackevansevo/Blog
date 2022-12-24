06f795950e765c8414dd217182046b8af6b8ff7d
Jack Evans <jack@evans.gb.net>
2022-12-24 12:34:04 +0000

    initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+AUTHOR = 'Jack Evans'
+SITENAME = "Jack's Blog"
+SITEURL = ''
+THEME = 'theme'
+DISPLAY_CATEGORIES_ON_MENU = False
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Resume', 'https://docs.google.com/document/d/e/2PACX-1vRHykkB3oIRnf7HkA6jlb_p_g47D0WPapwppQxyyss9AzXlLHqZTAT8qD0xnG7E6qSk7sNpF3GfdQdm/pub'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/ThisIsJackEvans'),
+          ('Github', 'https://github.com/Jackevansevo'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
